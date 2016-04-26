9b5a1934d8b74bb459872663d77f2c505c1410a4
Irina <irina1980311@mail.ru>
2016-04-25 20:50:47 -0400

    Улучшение кода тестов
    GroupModification+GroupDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,22 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTest extends TestBase {
-
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+    }
 
     @Test
     public void testGroupDeletion() {
 
-        app.getNavigationHelper().gotoGroupPage();
 
-        if (! app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         //int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup(before.size() - 1);
